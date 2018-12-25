5423d593a7cc7733e185d47bca26c9894a4b4888
pavel <pavelsuvit@gmail.com>
2018-12-25 22:13:14 +0300

    Move BindingAdapters from GameViewModel to it's own file

---
+++ app/src/main/java/com/wordpress/lonelytripblog/funwithflags/util/BindingAdapters.kt
@@ -0,0 +1,54 @@
+package com.wordpress.lonelytripblog.funwithflags.util
+
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.TransitionDrawable
+import android.os.Build
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.wordpress.lonelytripblog.funwithflags.R
+
+object BindingAdapters {
+
+    @JvmStatic
+    @BindingAdapter("setImage")
+    fun setImage(view: ImageView?, imageUrl: Int?) {
+        if (view == null || imageUrl == null) return
+        view.setImageResource(imageUrl)
+    }
+
+    @JvmStatic
+    @BindingAdapter("backgroundDrawable")
+    fun backgroundDrawable(view: Button, drawableResId: Int) {
+        val background = view.background
+        if (thisCanBeAnimated(drawableResId) && thisShouldBeAnimated(background)) {
+            animateView(background, view, drawableResId)
+        } else {
+            setDrawableForImage(view, ContextCompat.getDrawable(view.context, drawableResId)!!)
+        }
+    }
+
+    private fun setDrawableForImage(view: View, drawableToSet: Drawable) {
+        if (Build.VERSION.SDK_INT >= 16) {
+            view.background = drawableToSet
+        } else {
+            view.setBackgroundDrawable(drawableToSet)
+        }
+    }
+
+    private fun thisCanBeAnimated(drawableResId: Int) =
+            drawableResId == R.drawable.btn_background_chosen
+                    || drawableResId == R.drawable.btn_background_right_answer
+
+    private fun thisShouldBeAnimated(imageDrawable: Drawable) =
+            imageDrawable is TransitionDrawable || imageDrawable is GradientDrawable
+
+    private fun animateView(background: Drawable, view: Button, drawableResId: Int) {
+        val myDraw = TransitionDrawable(arrayOf(background, ContextCompat.getDrawable(view.context, drawableResId)!!))
+        setDrawableForImage(view, myDraw)
+        myDraw.startTransition(1000)
+    }
+}
--- app/src/main/java/com/wordpress/lonelytripblog/funwithflags/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/wordpress/lonelytripblog/funwithflags/viewmodels/GameViewModel.kt
@@ -1,14 +1,5 @@
 package com.wordpress.lonelytripblog.funwithflags.viewmodels
 
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.TransitionDrawable
-import android.os.Build
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
-import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.wordpress.lonelytripblog.funwithflags.R
@@ -89,47 +80,4 @@ open class GameViewModel @Inject constructor(private val gameRepository: GameRep
             lastChosenPosition = -1
         }
     }
-
-    companion object {
-
-        @JvmStatic
-        @BindingAdapter("setImage")
-        fun setImage(view: ImageView?, imageUrl: Int?) {
-            if (view == null || imageUrl == null) return
-            view.setImageResource(imageUrl)
-        }
-
-        @JvmStatic
-        @BindingAdapter("backgroundDrawable")
-        fun backgroundDrawable(view: Button, drawableResId: Int) {
-            val background = view.background
-            if (thisCanBeAnimated(drawableResId) && thisShouldBeAnimated(background)) {
-                animateView(background, view, drawableResId)
-            } else {
-                setDrawableForImage(view, ContextCompat.getDrawable(view.context, drawableResId)!!)
-            }
-        }
-
-        private fun setDrawableForImage(view: View, drawableToSet: Drawable) {
-            if (Build.VERSION.SDK_INT >= 16) {
-                view.background = drawableToSet
-            } else {
-                view.setBackgroundDrawable(drawableToSet)
-            }
-        }
-
-        private fun thisCanBeAnimated(drawableResId: Int) =
-                drawableResId == R.drawable.btn_background_chosen
-                        || drawableResId == R.drawable.btn_background_right_answer
-
-        private fun thisShouldBeAnimated(imageDrawable: Drawable) =
-                imageDrawable is TransitionDrawable || imageDrawable is GradientDrawable
-
-        private fun animateView(background: Drawable, view: Button, drawableResId: Int) {
-            val myDraw = TransitionDrawable(arrayOf(background, ContextCompat.getDrawable(view.context, drawableResId)!!))
-            setDrawableForImage(view, myDraw)
-            myDraw.startTransition(1000)
-        }
-    }
-
 }
