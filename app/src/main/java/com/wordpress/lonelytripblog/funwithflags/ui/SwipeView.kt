cfbe0ba54427564572b78d68a024d329ce9f7118
pavel <pavelsuvit@gmail.com>
2019-09-01 15:46:52 +0100

    Add empty SwipeView

---
+++ app/src/main/java/com/wordpress/lonelytripblog/funwithflags/ui/SwipeView.kt
@@ -0,0 +1,19 @@
+package com.wordpress.lonelytripblog.funwithflags.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class SwipeView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttribute: Int = 0)
+    : ViewGroup(context, attrs, defStyleAttribute) {
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+}
